3042d65afca1339a7f5a67b20cf9724ad8f1175f
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-07-18 20:01:29 +0100

    auth-middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,34 +1,31 @@
-import {JWT_SECRET} from '../config/env.js';
-import jwt from 'jsonwebtoken';  
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+import User from '../models/user.model.js';
 
 
-const authorize = async  (req , res , next) => {
-       //check if authorization exist and start with 'Bearer'
-        const authHeader = req.headers.authorization;
-        
-        if(!authHeader || !authHeader.startwith('Bearer ')){
-            return req.status(401).json({
-                success:false, 
-                message:'Access Denied, no token provided',
-            });
-        }
-        
-        const token = authHeader.split(' ')[1];
 
-    try {
-        //verify and decode token
-        const decoded = jwt.verify(token, JWT_SECRET);
+const authorize = async (req, res, next) => {
+  try {
+    let token;
 
-        //attach user info to request object 
-        req.user = decoded;
-        
-        //continue to next middlware route
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            success: false,
-            message: 'invalid or expired token',
-        });
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
     }
-};
+
+    if(!token) return res.status(401).json({ message: 'Unauthorized' });
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if(!user) return res.status(401).json({ message: 'Unauthorized' });
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized', error: error.message });
+  }
+}
+
 export default authorize;
