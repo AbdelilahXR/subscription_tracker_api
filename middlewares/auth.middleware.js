7ae0849cd3fb80c0d80017196e647f631657cc18
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-07-19 17:55:44 +0100

    implement auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,29 +3,46 @@ import { JWT_SECRET } from '../config/env.js';
 import User from '../models/user.model.js';
 
 
+const authorize = async  (req , res , next) => {
+       
+    try {   
+        //check if authorization exist and start with 'Bearer'
+        const authHeader = req.headers.authorization;
+        
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
+            return res.status(401).json({
+                message:'Access Denied, no token provided',
+            });
+        }
+        
+        const token = authHeader.split(' ')[1];
+        //verify and decode token
+        const decoded = jwt.verify(token, JWT_SECRET);
 
-const authorize = async (req, res, next) => {
-  try {
-    let token;
+        //attach user info to request object 
+        //req.user = decoded;
+        const user = await User.findById(decoded.userId);
+        if(!user){
+            return res.status(401).json({
+                success: 'false',
+                message: 'Unauthorized',
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
+            })
+        }
+        req.user = user;
+       
+        
+        //continue to next middlware route
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'invalid or expired token',
+        });
     }
-
-    if(!token) return res.status(401).json({ message: 'Unauthorized' });
-
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    const user = await User.findById(decoded.userId);
-
-    if(!user) return res.status(401).json({ message: 'Unauthorized' });
-
-    req.user = user;
-
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Unauthorized', error: error.message });
-  }
-}
-
+};
 export default authorize;
+
+
+
+
